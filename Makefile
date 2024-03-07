b99311bcd3a531201bb32a17d3eef57785698ddf
André Martins <andre.lucas.medeir@gmail.com>
2024-03-07 18:54:18 -0300

    feat: rename folders and change infra

--- Makefile
+++ Makefile
@@ -3,3 +3,9 @@ infra-up:
 
 infra-down:
 	cd ./infra && docker compose down
+
+scraper-service-up:
+	cd ./scraper-service && npm run build:start
+
+monitor-up:
+	cd ./monitor && npm run build:start
--- scraper-service/src/index.ts
+++ scraper-service/src/index.ts
@@ -1,6 +1,3 @@
-import "./opentelemetry/sdk";
-
-import { metrics } from "@opentelemetry/api";
 import ScraperList from "./schema/ScraperList";
 
 import GetCollegeHistory from "./scrapers/GetCollegeHistory";
@@ -10,12 +7,5 @@ const scrapersList = new ScraperList();
 
 scrapersList.add(new GetCollegeHistory());
 scrapersList.add(new GetJobsOnLinkedin());
-
-const metric = metrics.getMeter("test");
-
-const counter = metric.createCounter("webscrapers-add");
-counter.add(scrapersList.scrapers.length, {
-  names: scrapersList.scrapers.map((s) => s.name),
-});
 
 scrapersList.run();
