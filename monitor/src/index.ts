e5404f9de06113d463ae3ef29033b65fa289d6ae
André Martins <andre.lucas.medeir@gmail.com>
2024-03-07 09:56:04 -0300

    feat: create a folder monitor and start with opentelemetry sdk

--- monitor/src/index.ts
+++ monitor/src/index.ts
@@ -1,3 +1,6 @@
+import "./opentelemetry/sdk";
+
+import { metrics } from "@opentelemetry/api";
 import ScraperList from "./schema/ScraperList";
 
 import GetCollegeHistory from "./scrapers/GetCollegeHistory";
@@ -7,5 +10,12 @@ const scrapersList = new ScraperList();
 
 scrapersList.add(new GetCollegeHistory());
 scrapersList.add(new GetJobsOnLinkedin());
+
+const metric = metrics.getMeter("test");
+
+const counter = metric.createCounter("webscrapers-add");
+counter.add(scrapersList.scrapers.length, {
+  names: scrapersList.scrapers.map((s) => s.name),
+});
 
 scrapersList.run();
--- monitor/src/scrapers/GetCollegeHistory.ts
+++ monitor/src/scrapers/GetCollegeHistory.ts
@@ -1,9 +1,13 @@
 import Scraper from "../schema/Scraper";
 
+const delay = (amount = 750) =>
+  new Promise((resolve) => setTimeout(resolve, amount));
+
 export default class GetCollegeHistory implements Scraper {
   name = "get-college-history";
 
   async run() {
-    console.log(this.name);
+    await delay(5000);
+    console.log("oi");
   }
 }
