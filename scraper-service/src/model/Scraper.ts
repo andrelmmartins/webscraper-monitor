48b0919be7a889566ac4ab0721b4df068f6ee24e
André Martins <andre.lucas.medeir@gmail.com>
2024-03-21 18:32:36 -0300

    git: update branch

--- scraper-service/src/model/Scraper.ts
+++
@@ -1,6 +0,0 @@
-abstract class Scraper {
-  abstract name: string;
-  abstract run: () => Promise<void>;
-}
-
-export default Scraper;
--- scraper-service/src/scrapers/GetJobsOnLinkedin.ts
+++
@@ -1,9 +0,0 @@
-import Scraper from "../model/Scraper";
-
-export default class GetJobsOnLinkedin implements Scraper {
-  name = "get-jobs-on-linkedin";
-
-  async run() {
-    console.log(this.name);
-  }
-}
